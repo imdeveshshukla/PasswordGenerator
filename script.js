e7963dd34a5fc6680eceae957ff1421c40e38be7
Devesh Shukla <deveshshukla1603@gmail.com>
2023-09-01 16:23:44 +0530

    Some CSS

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const array = new Uint16Array(20);
 self.crypto.getRandomValues(array);
 
-console.log("Your lucky numbers:");
-for (const num of array) {
-  console.log(String.fromCodePoint(num));
-}
+// console.log("Your lucky numbers:");
+// for (const num of array) {
+//   console.log(String.fromCodePoint(num));
+// }
 
-//flex1
-//i have headache so i just want to flex
+var passSlider = document.querySelector('#passLength');
+var passLen = document.querySelector('#passLengText');
+function handleSlider() {
+  let i = passSlider.value;
+  // console.log(i);
+  passLen.textContent = i;
+}
+passSlider.addEventListener('input',handleSlider);
