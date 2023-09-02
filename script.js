47a34654c7870cca028795559e4d18f83b1adedb
Devesh Shukla <deveshshukla1603@gmail.com>
2023-09-02 15:41:38 +0530

    Add Js

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ var passupperCase = document.querySelector('#passUp');
 var passlowerCase = document.querySelector('#passlow');
 var passNumber = document.querySelector('#passNum');
 var passSym = document.querySelector('#passSymbol');
+var randomFunctions = [];
 var strengthIndicator = document.querySelector('#strengthIndicator');
+
 passSlider.addEventListener('input',handleSlider);
 
 function handleSlider() {
@@ -20,7 +22,7 @@ function handleSlider() {
   // console.log(i);
   passLen.textContent = i;
 }
-function getRandomNumberBtw(p,q) {
+function getRandomNumberBtw(p=0,q=9) {
   return Math.floor((Math.random())*(q-p)+p);
 }
 function getRandomLowerCase() {
@@ -42,3 +44,30 @@ function findStrength(){
   else  strengthIndicator.style = 'background-color:red';
 }
 
+function randomArrayValue() {
+  if(passupperCase.checked){
+    randomFunctions.push = getRandomUpperCase;
+    console.log(randomFunctions);
+  } 
+  if(passlowerCase.checked) randomFunctions.push = getRandomLowerCase;
+  if(passSym.checked) randomFunctions.push = getRandomSymbol;
+  if(passNumber.checked) randomFunctions.push = getRandomNumberBtw;
+
+  if(randomFunctions.length == 0) console.log("Error");
+  console.log(passupperCase.checked);
+}
+
+function generatePassword() {
+  randomArrayValue();
+  console.log(randomFunctions+" Length = "+randomFunctions.length);
+  let password = "";
+  let length = passSlider.value;
+  for(let i=1;i<=length;i++)
+  {
+    let k = getRandomNumberBtw(0,randomFunctions.length);
+    let callingFunction = randomFunctions[k];
+    let ch = callingFunction();
+    password = password+ch;
+  }
+  console.log(password);
+}
